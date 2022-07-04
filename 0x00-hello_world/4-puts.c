6ea403f1510ee05eae22d4abad63633712897732
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-03 22:19:59 -0700

    prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-   char test_str[] = "\"Programming is like building a multilingual puzzle";
+char test_str[] = "\"Programming is like building a multilingual puzzle"/n);
 
 	puts(test_str);
-        return (0);
+return (0);
 }
