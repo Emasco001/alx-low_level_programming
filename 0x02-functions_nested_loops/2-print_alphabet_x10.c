68a83bed35f9b9daf87cbc9e5567c27fa3dedb0d
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-06 00:03:30 -0700

    2 print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - print_alphabet_x10 - prints the alphabet x10 
- *
+ * main - print_alphabet_x10 - prints the alphabet x10
+ * 
  */
 
 void print_alphabet_x10(void)
@@ -18,4 +18,4 @@ int alpha2;
 	_putchar(alpha2);
 }
 	_putchar('\n');	}
-}	
+	}
