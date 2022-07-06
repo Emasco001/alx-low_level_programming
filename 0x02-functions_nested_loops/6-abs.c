7998d44a327d94be0a76e31ef4ac2e468fd9e404
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-06 11:27:43 -0700

    compute the abs value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - funtion that computes the value of an integer
+ *
+ * Return Always 0
+ */
+
+ int _abs(int);
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+return (0);
+}
