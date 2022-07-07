264f1740130125686119b0d42cc0c8f903174fe8
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-06 19:57:11 -0700

    print multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,13 @@
 int main(void)
 {
 int n, s  = 0;
-
 for (n = 3; n < 1024; n++)
-
-
-if ((n % 3 == 0) || (n % 5 == 0))
-
 {
-		s = s + n;
+if ((n % 3 == 0) || (n % 5 == 0))
+{
+	s = s + n;
 }
 }
-{		printf("%d\n", s);
-		return (0);
+	printf("%d\n", s);
+	return (0);
 }
