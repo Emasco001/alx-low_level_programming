923acf5184a167edf8979dee72929c26e28d65d1
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-06 20:10:54 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,10 +12,12 @@ int i;
 unsigned long a = 1;
 unsigned long b = 2;
 unsigned long c = 3;
+
 printf("1, 2, ");
+
 for (i = 0; i < 48; i++)
 {
-     c = a + b;
+c = a + b;
 printf("%ld", c);
 	a = b;
 	b = c;
