a6cca36510e815f05ec4517cc01f362951cdcb3e
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-08 21:02:54 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,16 @@
 
 void print_square(int size)
 {
-int a, b;
-if (size <= 0)
-_putchar('\n');
-for (a = 0; a < size; a++)
-{
-	for (b = 0; b < (size); b++)
-{
-        _putchar('#');
-}
-	_putchar('\n');
-}
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < (size); b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
