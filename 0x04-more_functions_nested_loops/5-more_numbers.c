286ac367edf627819004205d74e1bf4039def424
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-08 20:24:16 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,15 @@ for (i = 0; i < 10; i++)
 {
 for (c = 0; c <= 14; c++)
 {
-if (c < 10)
+	if (c < 10)
 		b = c;
 else
 {
-		a = c / 10; b = c % 10;
+		a = c / 10;
+		b = c % 10;
 		_putchar('0' + a);
 }
-                _putchar('0' + b);
+_putchar('0' + b);
 }
 		_putchar('\n');
 }
