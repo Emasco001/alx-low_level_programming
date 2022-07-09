6e85b9b6da7325245bc61f1e3637bc8a09ab3761
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-08 18:51:39 -0700

    putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,3 @@
-_putchar
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +8,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+_putchar
