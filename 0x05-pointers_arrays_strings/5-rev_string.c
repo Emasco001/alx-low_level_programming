7896ba23232c55f4421ae6cdc5c22c85eb319646
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-12 11:26:52 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,19 +12,22 @@ int i;
 int j;
 int temp;
 int length;
+	
 	while (s[i] != 0)
-{
+	{	
 	i++;
-}
+	}
+	
 	length = i;
 	i = 0;
 	j = length - 1;
+	
 	while (i < j)
-{
+	{	
 	temp = s[i];
 	s[i] = s[j];
 	s[j] = temp;
 	i++;
 	j--;
-}
+	}
 }
