3c8d29c26795be1cee7bee4c7a87f28c30a9fe01
Emmanuel Ufiah <malachy820@gmail.com>
2022-07-13 21:51:20 -0700

    strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * rev_string - reverses a string
  * @s: holi
  *
  */
-
 void rev_string(char *s)
 {
 int i;
@@ -14,20 +12,18 @@ int temp;
 int length;
 	
 	while (s[i] != 0)
-	{	
+{	
 	i++;
-	}
-	
+}
 	length = i;
 	i = 0;
 	j = length - 1;
-	
 	while (i < j)
-	{	
+{	
 	temp = s[i];
 	s[i] = s[j];
 	s[j] = temp;
 	i++;
 	j--;
-	}
+}
 }
